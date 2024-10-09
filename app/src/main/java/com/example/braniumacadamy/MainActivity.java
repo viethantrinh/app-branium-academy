b646d70d93ce41496a679346f328565185947c4a
Dao Minh Quang <minhquang020903@gmail.com>
2024-10-09 10:56:19 +0700

    complete user UI

--- app/src/main/java/com/example/braniumacadamy/MainActivity.java
+++ app/src/main/java/com/example/braniumacadamy/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.braniumacadamy;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,21 +12,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        btnLogin = findViewById(R.id.btn_login);
+        setContentView(R.layout.frag_update_avatar);
 
 
-        btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(this, VerificationActivity.class);
-            startActivity(intent);
-        });
+//        btnLogin = findViewById(R.id.btn_login);
+
+
+//        btnLogin.setOnClickListener(v -> {
+//            Intent intent = new Intent(this, VerificationActivity.class);
+//            startActivity(intent);
+//        });
 
 
     }
